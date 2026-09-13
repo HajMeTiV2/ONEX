cae9f0e37b7d57451f338849100c25ad72add7c9
HajMeTiV2 <hajmeti@hotmail.com>
2026-09-13 20:36:14 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,9 @@
-
 #!/bin/bash
 
-# تنظیم پورت ریلوی روی Nginx
+# جایگزینی پورت در تنظیمات Nginx
 envsubst '$PORT' < /app/nginx.conf.template > /etc/nginx/sites-available/default
 
-# ساخت دیتابیس و تنظیم کانفیگ‌های Xray
+# ساخت دیتابیس و کانفیگ Xray
 python -c "from app import init_db, sync_xray_config; init_db(); sync_xray_config()"
 
 # اجرای Xray در پس‌زمینه
@@ -13,5 +12,5 @@ xray -config /app/xray_config.json &
 # اجرای Nginx در پس‌زمینه
 nginx -g "daemon off;" &
 
-# اجرای برنامه Flask Panel
+# اجرای پنل Flask
 exec gunicorn app:app --bind 127.0.0.1:5000 --workers 2

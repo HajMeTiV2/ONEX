a56cdc705287b857d9356151b60a970c4f7afddd
HajMeTiV2 <hajmeti@hotmail.com>
2026-09-14 01:24:16 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,14 @@
 # تنظیم پورت Nginx
 envsubst '$PORT' < /app/nginx.conf.template > /etc/nginx/sites-available/default
 
-# اجرای Nginx در پس‌زمینه
-nginx &
-
-# ساخت دیتابیس و روشن کردن Xray
+# ساخت دیتابیس اولیه و کانفیگ Xray
 python -c "from app import init_db, sync_xray_config; init_db(); sync_xray_config()"
 
-# اجرای وب سرور اصلی
+# ۱. اجرای سرور Nginx در پس‌زمینه
+nginx &
+
+# ۲. اجرای مستقیم و مستقل هسته Xray در پس‌زمینه
+/usr/local/bin/xray -config /app/xray_config.json &
+
+# ۳. اجرای پنل اصلی
 exec gunicorn app:app --bind 127.0.0.1:5000 --workers 2

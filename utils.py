c6f356add82627c4b1357216774bb3fd913a93f8
HajMeTiV2 <hajmeti@hotmail.com>
2026-09-13 19:49:03 +0330

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,15 +1,23 @@
-
 import json
 import base64
 import urllib.parse
 from config import Config
 
 
-def generate_vless_config(user):
+def get_server_address(request_domain=None):
+    """اگر آدرس سرور تنظیم نشده باشد، از دامنه ریلوی استفاده میکند"""
+    if Config.XRAY_ADDRESS and Config.XRAY_ADDRESS != 'your-server-ip':
+        return Config.XRAY_ADDRESS
+    if request_domain:
+        return request_domain
+    return Config.PANEL_DOMAIN
+
+
+def generate_vless_config(user, request_domain=None):
     uuid_str = user.uuid_str
-    address = Config.XRAY_ADDRESS
+    address = get_server_address(request_domain)
     port = Config.XRAY_PORT
-    sni = Config.XRAY_SNI
+    sni = Config.XRAY_SNI if Config.XRAY_SNI != 'www.speedtest.net' else address
     path = Config.XRAY_PATH
     network = Config.XRAY_NETWORK
     security = Config.XRAY_SECURITY
@@ -25,34 +33,30 @@ def generate_vless_config(user):
         'encryption': 'none'
     }
 
-    if security == 'reality':
-        params['pbk'] = Config.__dict__.get('XRAY_PUBLIC_KEY', '')
-        params['sid'] = Config.__dict__.get('XRAY_SHORT_ID', '')
-        params['flow'] = 'xtls-rprx-vision'
-        params.pop('path', None)
-        params.pop('host', None)
-
     params_str = '&'.join(f'{k}={v}' for k, v in params.items())
     remark = urllib.parse.quote(f'ONEX-{user.username}')
 
     return f"vless://{uuid_str}@{address}:{port}?{params_str}#{remark}"
 
 
-def generate_vmess_config(user):
+def generate_vmess_config(user, request_domain=None):
+    address = get_server_address(request_domain)
+    sni = Config.XRAY_SNI if Config.XRAY_SNI != 'www.speedtest.net' else address
+
     config_dict = {
         "v": "2",
         "ps": f"ONEX-{user.username}",
-        "add": Config.XRAY_ADDRESS,
+        "add": address,
         "port": str(Config.XRAY_PORT),
         "id": user.uuid_str,
         "aid": "0",
         "scy": "auto",
         "net": Config.XRAY_NETWORK,
         "type": "none",
-        "host": Config.XRAY_SNI,
+        "host": sni,
         "path": Config.XRAY_PATH,
         "tls": Config.XRAY_SECURITY,
-        "sni": Config.XRAY_SNI,
+        "sni": sni,
         "alpn": "",
         "fp": Config.XRAY_FINGERPRINT
     }
@@ -62,11 +66,11 @@ def generate_vmess_config(user):
     return f"vmess://{b64}"
 
 
-def generate_trojan_config(user):
+def generate_trojan_config(user, request_domain=None):
     password = user.uuid_str
-    address = Config.XRAY_ADDRESS
+    address = get_server_address(request_domain)
     port = Config.XRAY_PORT
-    sni = Config.XRAY_SNI
+    sni = Config.XRAY_SNI if Config.XRAY_SNI != 'www.speedtest.net' else address
     path = Config.XRAY_PATH
     network = Config.XRAY_NETWORK
 
@@ -85,17 +89,17 @@ def generate_trojan_config(user):
     return f"trojan://{password}@{address}:{port}?{params_str}#{remark}"
 
 
-def generate_config(user):
+def generate_config(user, request_domain=None):
     generators = {
         'vless': generate_vless_config,
         'vmess': generate_vmess_config,
         'trojan': generate_trojan_config
     }
-    return generators.get(user.protocol, generate_vless_config)(user)
+    return generators.get(user.protocol, generate_vless_config)(user, request_domain)
 
 
-def generate_subscription_content(user):
-    config = generate_config(user)
+def generate_subscription_content(user, request_domain=None):
+    config = generate_config(user, request_domain)
     return base64.b64encode(config.encode()).decode()
 
 
